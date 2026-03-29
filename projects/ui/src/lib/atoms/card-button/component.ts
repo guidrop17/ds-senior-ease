31cee07674c2411f00774fcbe1b1e5435244c3dd
Guilherme Araujo <guidrop17@gmail.com>
2026-03-29 18:40:56 -0300

    fix: enhance card button component to support template references for icons and descriptions

--- projects/ui/src/lib/atoms/card-button/component.ts
+++ projects/ui/src/lib/atoms/card-button/component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, TemplateRef } from '@angular/core';
 import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 import { IconComponent, IconName, IconValue } from '../icon';
 
@@ -12,9 +12,11 @@ import { IconComponent, IconName, IconValue } from '../icon';
 })
 export class CardButtonComponent {
   @Input() title = '';
-  @Input() description = '';
-  @Input() leadingIcon: IconValue | null = null;
-  @Input() trailingIcon: IconValue | null = null;
+  @Input() description: string | TemplateRef<any> = '';
+  @Input() leading: IconValue | TemplateRef<any> | null = null;
+  @Input() useLeadingIcon = false;
+  @Input() trailing: IconValue | TemplateRef<any> | null = null;
+  @Input() useTrailingIcon = false;
   @Input() iconContainerClass: string | string[] | Record<string, boolean> = '';
   @Input() disabled = false;
   @Input() type: 'button' | 'submit' | 'reset' = 'button';
@@ -22,12 +24,16 @@ export class CardButtonComponent {
 
   @Output() buttonClick = new EventEmitter<MouseEvent>();
 
-  protected resolveIconName(icon: IconValue | null): IconName {
+  protected isTemplate(value: any): boolean {
+    return value instanceof TemplateRef;
+  }
+
+  protected resolveIconName(icon: IconValue | TemplateRef<any> | null): IconName {
     return typeof icon === 'string' ? icon : 'home';
   }
 
-  protected resolveIconDefinition(icon: IconValue | null): IconDefinition | null {
-    return typeof icon === 'string' || icon === null ? null : icon;
+  protected resolveIconDefinition(icon: IconValue | TemplateRef<any> | null): IconDefinition | null {
+    return typeof icon === 'string' || icon === null ? null : icon instanceof TemplateRef ? null : (icon as IconDefinition);
   }
 
   protected emitClick(event: MouseEvent): void {
